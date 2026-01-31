3c7cf33dc5b59b42c5ea636b50b2a3d331b4e378
Adolfo Brunno <adolfo.brunno.ba@gmail.com>
2026-01-31 15:20:55 -0300

    Removendo prompt para checar tipo da resposta

--- src/main/java/com/abba/tanahora/application/service/ReminderEventServiceImpl.java
+++ src/main/java/com/abba/tanahora/application/service/ReminderEventServiceImpl.java
@@ -1,16 +1,14 @@
 package com.abba.tanahora.application.service;
 
+import com.abba.tanahora.application.dto.MessageReceivedType;
 import com.abba.tanahora.domain.model.Reminder;
 import com.abba.tanahora.domain.model.ReminderEvent;
 import com.abba.tanahora.domain.model.ReminderEventStatus;
 import com.abba.tanahora.domain.model.User;
 import com.abba.tanahora.domain.repository.ReminderEventRepository;
 import com.abba.tanahora.domain.service.ReminderEventService;
-import com.abba.tanahora.domain.service.ReminderService;
 import com.abba.tanahora.domain.service.UserService;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
@@ -21,14 +19,10 @@ import java.util.Optional;
 public class ReminderEventServiceImpl implements ReminderEventService {
 
     private final ReminderEventRepository reminderEventRepository;
-    private final OpenAiApiService openAiApiService;
-    private final ReminderService reminderService;
     private final UserService userService;
 
-    public ReminderEventServiceImpl(ReminderEventRepository reminderEventRepository, OpenAiApiService openAiApiService, ReminderService reminderService, UserService userService) {
+    public ReminderEventServiceImpl(ReminderEventRepository reminderEventRepository, UserService userService) {
         this.reminderEventRepository = reminderEventRepository;
-        this.openAiApiService = openAiApiService;
-        this.reminderService = reminderService;
         this.userService = userService;
     }
 
@@ -67,7 +61,7 @@ public class ReminderEventServiceImpl implements ReminderEventService {
             event = reminderEventRepository.findFirstByWhatsappMessageId(replyToMessageId);
         }
 
-        ReminderEventStatus reminderEventStatus = parseStatus(responseText).orElse(ReminderEventStatus.PENDING);
+        ReminderEventStatus reminderEventStatus = resolveStatus(responseText);
 
         log.debug("Updating reminder event {} status to {}", event, reminderEventStatus);
 
@@ -81,23 +75,10 @@ public class ReminderEventServiceImpl implements ReminderEventService {
         return event;
     }
 
-    private Optional<ReminderEventStatus> parseStatus(String responseText) {
-        if (responseText == null || responseText.isBlank()) {
-            return Optional.empty();
-        }
+    private ReminderEventStatus resolveStatus(String responseText) {
 
-        ReminderEventStatusDTO reminderEventStatusDTO = openAiApiService.sendPrompt(String.format(
-                """
-                        Analise a seguinte mensagem e retorne um status adequado de acordo com o modelo.
-                        
-                        mensagem = %s
-                        """, responseText), ReminderEventStatusDTO.class);
+        MessageReceivedType messageReceivedType = MessageReceivedType.valueOf(responseText);
 
-        return Optional.ofNullable(reminderEventStatusDTO.status);
-    }
-
-    static class ReminderEventStatusDTO {
-        @JsonProperty(required = true)
-        ReminderEventStatus status;
+        return messageReceivedType == MessageReceivedType.REMINDER_RESPONSE_TAKEN ? ReminderEventStatus.TAKEN : ReminderEventStatus.SKIPPED;
     }
 }
